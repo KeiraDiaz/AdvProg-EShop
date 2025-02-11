487f683f80123eea789f883b0aab43855fe976e2
KeiraDiaz <nkeiradiaz@gmail.com>
2025-02-11 09:14:54 +0700

    test: added CreateProductFunctionalTest

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -0,0 +1,52 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+class CreateProductFunctionalTest {
+
+    @LocalServerPort
+    private int serverPort;
+
+    @Value("${app.baseUrl:http://localhost}")
+    private String testBaseUrl;
+
+    private String baseUrl;
+
+    @BeforeEach
+    void setupTest() {
+        baseUrl = String.format("%s:%d/product/create", testBaseUrl, serverPort);
+    }
+
+    @Test
+    void testCreateProduct(ChromeDriver driver) {
+        driver.get(baseUrl);
+
+        WebElement nameInput = driver.findElement(By.id("nameInput"));
+        WebElement quantityInput = driver.findElement(By.id("quantityInput"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button.btn-primary"));
+
+        String productName = "Test Product";
+        int productQuantity = 10;
+        nameInput.sendKeys(productName);
+        quantityInput.sendKeys(String.valueOf(productQuantity));
+
+        submitButton.click();
+
+    }
+}
