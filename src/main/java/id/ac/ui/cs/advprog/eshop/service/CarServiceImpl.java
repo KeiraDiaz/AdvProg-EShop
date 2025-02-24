0fd6603f1e1c97565ec1fff31b27fba33a516242
KeiraDiaz <nkeiradiaz@gmail.com>
2025-02-24 17:45:23 +0700

    fix: CarService not found becuase file naming error :(

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarServiceImpl.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.service.CarService;
 import id.ac.ui.cs.advprog.eshop.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
