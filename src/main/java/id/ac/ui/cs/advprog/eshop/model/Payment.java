e4442e78db153382fd3de1c9512b50d18b91eff9
KeiraDiaz <nkeiradiaz@gmail.com>
2025-03-05 23:08:02 +0700

    [RED] Add Payment Model Skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Payment {
+    private String id;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
+}
